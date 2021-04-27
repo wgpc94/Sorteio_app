8bbd23a6ff92145ce997ce355e205b0b1b770127
Welington Gomes <Welingtonsc94@gmail.com>
2021-04-27 20:23:02 -0300

    layout activity_main, activity_sorteio_numeros and toolbar

---
+++ app/src/main/java/com/example/sorteio_app/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.sorteio_app
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+    }
+}
---
+++ app/src/main/java/com/example/sorteio_app/SorteioNumeros.kt
@@ -0,0 +1,16 @@
+package com.example.sorteio_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class SorteioNumeros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sorteio_numeros)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
